096921b21ccf59a0a7f9437ea773dda60cdd1459
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-07-17 21:25:36 +0300

    first test was created

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,23 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class FirstTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browser = "chrome";
+    }
+
+    @Test
+    void firstTest() {
+        open("https://google.com");
+        System.out.println("firstTest()");
+        Assertions.assertTrue(3 > 2);
+    }
+
+
+}
