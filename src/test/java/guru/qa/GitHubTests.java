52cd90bd4c1f115db2e161c4f39743ef658a25d8
Valeria Reshetina <dmitry.potapov@icloud.com>
2023-07-28 11:34:53 +0300

    gitHubSoftAssertionsTest for Lesson#4 was redone. gitHubHoverTest and gitHubDragAndDropTest for Lesson#5 were created

--- src/test/java/guru/qa/GitHubTests.java
+++ src/test/java/guru/qa/GitHubTests.java
@@ -3,11 +3,13 @@ package guru.qa;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import jdk.jfr.Description;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitHubTests {
@@ -18,14 +20,48 @@ public class GitHubTests {
         Configuration.pageLoadStrategy = "eager";
     }
 
+    @Description("Test for lesson #4 to check that there is example code for JUnit5")
     @Test
     public void gitHubSoftAssertionsTest() {
+        String codeExample =  "@ExtendWith({SoftAssertsExtension.class})\n" +
+                "class Tests {\n" +
+                "  @Test\n" +
+                "  void test() {\n" +
+                "    Configuration.assertionMode = SOFT;\n" +
+                "    open(\"page.html\");\n" +
+                "\n" +
+                "    $(\"#first\").should(visible).click();\n" +
+                "    $(\"#second\").should(visible).click();\n" +
+                "  }\n" +
+                "}";
         open("https://github.com/selenide/selenide");
+
         $x("//*[@id='wiki-tab']").click();
         $x("//*[@class='Box-row wiki-more-pages-link']//button").click();
         $x("//*[@id='wiki-pages-box']//*[contains(text(), 'SoftAssertions')]")
                 .shouldBe(Condition.interactable).scrollTo().click();
-        $x("//*[@id='wiki-body']").shouldHave(text("Using JUnit5 extend test class"));
+        $x("//div[@class='markdown-body']").$(byText("3. Using JUnit5 extend test class:"))
+                .sibling(0).shouldHave(text(codeExample));
+    }
+
+    @Description("First test for lesson #5 to check that necessary page has loaded by it's title")
+    @Test
+    public void gitHubHoverTest() {
+        open("https://github.com/");
+
+        $x("//*[contains(text(), 'Solutions')]").hover();
+        $x("//*[contains(text(), 'Enterprise')]").click();
+        $x("//*[@class='application-main ']").shouldHave(text("Build like the best"));
+    }
+
+    @Description("Second test for lesson #5 to check that rectangles A and B are indeed switched places")
+    @Test
+    public void gitHubDragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        $x("//div[@id='column-a']").dragAndDropTo($x("//div[@id='column-b']"));
+        $x("//div[@id='column-a']").shouldHave(text("B"));
+        $x("//div[@id='column-b']").shouldHave(text("A"));
     }
 
     @AfterAll
